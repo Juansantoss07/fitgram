897a7934528f2cb7f6cfd6d6a69baf1669f385ee
Juansantoss07 <juansantosscunha@gmail.com>
2024-04-25 23:35:57 -0300

    adicionado app de comentarios nas postagens e melhorado animações

--- templates/static/js/scroll_reveal.js
+++ templates/static/js/scroll_reveal.js
@@ -7,3 +7,4 @@ ScrollReveal().reveal('.container-login-cadastro',  {delay:0, origin:'left', mob
 ScrollReveal().reveal('.register',  {delay:0, origin:'right', mobile: false});
 ScrollReveal().reveal('.sidebar',  {delay:0, origin:'left', mobile: false});
 ScrollReveal().reveal('.home',  {delay:0, origin:'top', mobile: false});
+
